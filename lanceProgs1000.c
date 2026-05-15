c2b3e5616a6334a82524d3c9db9eb25474e76b4e
TheStriker <anasrifakofficial@gmail.com>
2026-05-15 04:20:23 +0100

    final version of launch args N times (N = 10 at the moment)

--- lanceProgs1000.c
+++ lanceProgs1000.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 #define N 10
+#define FAILED_FORK -100
+#define FAILED_EXEC -101
 
 int main(int argc, char** argv) {
 
     int p;
-
+    char* emptyArgList[0] = {};
     for (int i = 1; i < argc; i++) {
         for (int j = 0; j < N; j++) {
-            p = fork();
-            switch (p) {
+            switch (fork()) {
                 case -1:
+                    perror("fork");
+                    exit(FAILED_FORK);
                     break;
-                case 0:
-                    printf("launching: %s\n", argv[i]);
-                    execlp(argv[0],NULL);
-                    printf("failed to launch: %s\n", argv[i]);
+                case 0:   
+                    execlp(argv[i],argv[i], NULL);
+                    perror("exec");
+                    exit(FAILED_EXEC);
                     break;
                 //je suis le parent
             }
 
         }
     }
+
+    while(wait(NULL) > 0);//retourn -1 lorsqu'il n'y a pas de processus enfants
+
     return 0;
 }
